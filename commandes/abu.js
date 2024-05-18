359fa3df59ea732b2651a8596677dc86956d2bf1
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-05-18 09:23:28 +0100

    Update abu.js

--- commandes/abu.js
+++ commandes/abu.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
 france({ nomCom: "abu", reaction: "🚗", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝗢𝘇𝗮𝗮𝗵👊 𝗧𝗵𝗶𝘀 𝗶𝘀 *𝗕𝗠𝗪-𝗠𝗗*  𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗯𝘆 *𝗜𝗕𝗥𝗔𝗛𝗜𝗠 𝗔𝗗𝗔𝗠𝗦*🥷 𝗣𝗹𝗲𝗮𝘀𝗲 𝗚𝗶𝘃𝗲 𝗮 𝗦𝘁𝗮𝗿 ✨ 𝗢𝗻 𝗧𝗵𝗲 𝗥𝗲𝗽𝗼 𝗔𝗻𝗱 𝗙𝗼𝗿𝗸 𝗜𝘁 🚗 \n\n ' + "║❒ 𝗥𝗲𝗽𝗼 https://github.com/ibrahimaitech/IBRAHIM-AI-10.10";
-    let d = '       ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@ibrahimmdgpt';
+    let z = '𝐌𝐲 𝐝𝐞𝐚𝐫 😍 𝐓𝐡𝐢𝐬 𝐢𝐬 *𝐓𝐎𝐆𝐄-𝐌𝐃*  𝐂𝐫𝐞𝐚𝐭𝐞𝐝 𝐛𝐲 *𝙏𝙊𝙂𝙀 𝙄𝙉𝙐𝙈𝘼𝙆𝙄*🥷 𝗣𝗹𝗲𝗮𝘀𝗲 𝗚𝗶𝘃𝗲 𝗮 𝗦𝘁𝗮𝗿 ✨ 𝗢𝗻 𝗧𝗵𝗲 𝗥𝗲𝗽𝗼 𝗔𝗻𝗱 𝗙𝗼𝗿𝗸 𝗜𝘁 🚗 \n\n ' + "𝐑𝐞𝐩𝐨 https://github.com/toge012345/TOGE-V2";
+    let d = '𝐆𝐫𝐨𝐮𝐩 https://chat.whatsapp.com/JQ4s2pJuBReE7YL9wKJPHo';
     let varmess = z + d;
     var mp3 = 'https://s31.aconvert.com/convert/p3r68-cdx67/7uiwm-6zr16.mp3'';
     await zk.sendMessage(dest, { audio: { url: mp3 }, caption: varmess });
@@ -20,8 +20,8 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *BMW-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *BMW-MD-REPO*'
+   let z ='Salut je m\'appelle *TOGE-V2-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *TOGE-V2-REPO*'
       let varmess=z+d
       var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});

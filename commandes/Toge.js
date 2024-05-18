014e16b0d2a932dd188c22efe371ea6ec2119203
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-05-18 17:03:29 +0100

    Update and rename Ibrahim.js to Toge.js

--- commandes/Toge.js
+++ commandes/Toge.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
-france({ nomCom: "ibrahim", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+france({ nomCom: "Toge", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Yes you called me whats up Ok anyway support me by following my channel and subscribing on my youtube channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
-    let d = '                                     ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@ibrahimaitech';
+    let z = 'Yes you called me whats up Ok anyway support me by following my channel and subscribing on my whatsapp channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VaYtF0Z2975CYz9Fmv2k";
+    let d = '                                     ║❒ 𝐆𝐫𝐨𝐮𝐩 https://chat.whatsapp.com/JQ4s2pJuBReE7YL9wKJPHo';
     let varmess = z + d;
-    var mp4 = 'https://telegra.ph/file/dec033e457813cd2ff2e3.mp4';
+    var jpg = 'https://telegra.ph/file/ee1d72d6e59b22b99216e.jpg';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
     //console.log("montest")
 });
